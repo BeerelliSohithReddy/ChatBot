3e64a61bc40839d7a4799775ce03c42d95dc03e0
Sohith Reddy <162091459+BeerelliSohithReddy@users.noreply.github.com>
2024-03-20 17:52:18 +0530

    Add files via upload

---
+++ chatbotproject.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 10:23:10 2023
+
+@author: sohith
+"""
+
+from nltk.chat.util import Chat, reflections
+pairs = [
+    [
+        r"(.*)my name is (.*)",
+        ["Hello %2, How are you today ?",]
+    ],
+    [
+        r"(.*)help(.*) ",
+        ["I can help you ",]
+    ],
+     [
+        r"(.*) your name ?",
+        ["My name is the chatbot, but you can just call me robot and I'm a chatbot .",]
+    ],
+    [
+        r"how are you (.*) ?",
+        ["I'm doing very well", "i am great !"]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind that",]
+    ],
+    [
+        r"i'm (.*) (good|well|okay|ok)",
+        ["Nice to hear that","Alright, great !",]
+    ],
+    [
+        r"(hi|hey|hello|hola|holla)(.*)",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+        
+    ],
+    [
+        r"(.*)created(.*)",
+        ["sohith created me using Python's NLTK library ","top secret ;)",]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['hyderabad, India',]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain in the past 4 days here in %2","In %2 there is a 50% chance of rain",]
+    ],
+    [
+        r"how (.*) health (.*)",
+        ["Health is very important, but I am a computer, so I don't need to worry about my health ",]
+    ],
+    [
+        r"(.*)(sports|game|sport)(.*)",
+        ["I'm a very big fan of Cricket",]
+    ],
+    [
+        r"who (.*) (Cricketer|Batsman)?",
+        ["Virat Kohli"]
+    ],
+    [
+        r"quit",
+        ["Bye for now. See you soon :) ","It was nice talking to you. See you soon :)"]
+    ],
+    [
+        r"(.*)",
+        ['our customer service will reach you']
+    ],
+ ]   
+my_dummy_reflections= {
+    "go"     : "gone",
+    "hello"    : "hey there"
+}
+print("Hi, I'm the chatbot and I like to chat\nPlease type lowercase English language to start a conversation. Type quit to leave ")
+#Create Chat Bot
+chat = Chat(pairs, reflections)
+
+
+chat.converse()
